3b3b6aeb27f6dada696aa2ec951ba3166799bdc6
saint-thomas-more <saint-thomas-more@github.com>
2020-07-30 18:08:35 +0100

    Initial commit.

---
+++ Person.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mincrypt
+{
+    public class Person
+    {
+
+        private RSACryptoServiceProvider RSA;
+        private ECDiffieHellmanCng ECDH;
+        private SHA256CryptoServiceProvider SHA2;
+        private byte[] symmettricKey;
+
+        public Person()
+        {
+            // Generate a 2048-bit RSA public-private pair.
+            RSA = new RSACryptoServiceProvider(2048);
+            SHA2 = new SHA256CryptoServiceProvider();
+
+            // Setup an ECDH implementation.
+            ECDH = new ECDiffieHellmanCng();
+            ECDH.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+            ECDH.HashAlgorithm = CngAlgorithm.Sha256;
+        }
+
+        public RSAParameters GetPublicRSAParameters()
+        {
+            return RSA.ExportParameters(false);
+        }
+
+        public byte[] GetSignedDHPublicKey()
+        {
+            return RSA.SignData(GetDHPublicKey().ToByteArray(), SHA2);
+        }
+
+        public ECDiffieHellmanPublicKey GetDHPublicKey()
+        {
+            return ECDH.PublicKey;
+        }
+
+        public bool InitaliseSymmettricKey(ECDiffieHellmanPublicKey otherPartyPublicKey, byte[] signedPublicKey, RSAParameters otherPartyRSAPublicKey)
+        {
+            RSACryptoServiceProvider csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(otherPartyRSAPublicKey);
+            if(csp.VerifyData(otherPartyPublicKey.ToByteArray(), SHA2, signedPublicKey))
+            {
+                symmettricKey = ECDH.DeriveKeyMaterial(otherPartyPublicKey);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Encrypt(string secretMessage, out byte[] encryptedMessage, out byte[] iv)
+        {
+            using (Aes aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = symmettricKey;
+                iv = aes.IV;
+
+                // Encrypt the message
+                using (MemoryStream ciphertext = new MemoryStream())
+                using (CryptoStream cs = new CryptoStream(ciphertext, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    byte[] plaintextMessage = Encoding.UTF8.GetBytes(secretMessage);
+                    cs.Write(plaintextMessage, 0, plaintextMessage.Length);
+                    cs.Close();
+                    encryptedMessage = ciphertext.ToArray();
+                }
+            }
+        }
+
+        public string Decrypt(byte[] encryptedMessage, byte[] iv)
+        {
+            using (Aes aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = symmettricKey;
+                aes.IV = iv;
+                // Decrypt the message
+                using (MemoryStream plaintext = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(plaintext, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(encryptedMessage, 0, encryptedMessage.Length);
+                        cs.Close();
+                        return Encoding.UTF8.GetString(plaintext.ToArray());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using mincrypt;
+
+namespace MinCrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person alice = new Person();
+            Person bob = new Person();
+
+            Console.WriteLine("Exchanging keys...");
+
+            // The DH public key is signed.
+            byte[] aliceSignedDHPublicKey = alice.GetSignedDHPublicKey();
+            byte[] bobSignedDHPublicKey = bob.GetSignedDHPublicKey();
+
+            // The DH public key is verified and then used to derive a symmettric key for message encryption.
+            bool aliceInitialised = alice.InitaliseSymmettricKey(bob.GetDHPublicKey(), bobSignedDHPublicKey, bob.GetPublicRSAParameters());
+            bool bobInitialised = bob.InitaliseSymmettricKey(alice.GetDHPublicKey(), aliceSignedDHPublicKey, alice.GetPublicRSAParameters());
+
+            if (!aliceInitialised || !bobInitialised)
+            {
+                Console.WriteLine("Key exchange and verification failed.");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("Keys exchanged.");
+
+            byte[] encryptedMessage = null;
+            byte[] iv = null;
+
+            Console.Write("Enter a message for Alice to send: ");
+            var originalMessage = Console.ReadLine();
+            Console.WriteLine("Sending message \"" + originalMessage + "\".");
+
+            alice.Encrypt(originalMessage, out encryptedMessage, out iv);
+            Console.WriteLine("Alice sent \"" + Encoding.Default.GetString(encryptedMessage) + "\".");
+
+            string decryptedMessage = bob.Decrypt(encryptedMessage, iv);
+            Console.WriteLine("Bob decrypted \"" + decryptedMessage + "\".");
+        }
+    }
+}
